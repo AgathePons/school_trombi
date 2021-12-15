8c27883dbffcff3fd3c19a3f70550526f30e6f4c
AgathePons <ponsagathe37@gmail.com>
2021-12-15 13:54:49 +0100

    router + home, 404 views

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 require('dotenv').config();
-
 const express = require('express');
+const router = require('./app/router');
 
 const app = express();
-
 const PORT = process.env.PORT || 5050;
 
+app.set('views', './app/views');
+app.set('view engine', 'ejs');
+
+app.use(express.static('./public'));
+
+app.use(router);
+
+app.use((req, res) => {
+    res.status(404).render('404');
+});
+
 app.listen(PORT, () => {
-    console.log(`listening on ${PORT}, visit http://localhost:${PORT}`);
+    console.log(`Listening on ${PORT}, visit http://localhost:${PORT} ...`);
 });
