55fcc006824d208d08c57c84a0fd13eaa4742962
AgathePons <ponsagathe37@gmail.com>
2021-12-16 14:55:16 +0100

    test pg

---
+++ test.js
@@ -0,0 +1,22 @@
+const {
+  Client
+} = require('pg');
+
+// ^ on va chercher direct Client de base
+// const pg = require('pg');
+// const client = pg.client;
+
+
+// on crée un client de connexion à la bdd avec une connection string
+//<protocol>://<user>:<pswd>@<host>:<port>/<database>
+const client = new Client('postgresql://etudiant:js4life@pg.oclock.lan:5432/trombi');
+
+client.connect();
+
+const resultats = client.query('SELECT * FROM "promo" LIMIT 10', (err, resultats) => {
+  if(err) {
+    return console.error('Oupsiii', err);
+  }
+  console.log(resultats.rows);
+
+});
